deaa5918834e3f5b86d6069feddf34032c3059f7
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-07-26 16:07:38 -0400

    update

--- sever.js
+++ sever.js
@@ -1,21 +1,55 @@
-// Require Dependencies
 const express = require("express");
 const fs = require("fs");
-const path = require('path');
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
 
-// Initialize express app
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4023;
 
-// Setup data parsing
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
-//Require routes file
-require('./routes/routes')(app);
+//Setting routes for APIs
+//This gets notes saved and joins it in db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
 
-// Setup listener
-app.listen(PORT, function() {
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+//used for deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+
+//HTML calls
+//calls home page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//call for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//Start listen
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-});  
+});
