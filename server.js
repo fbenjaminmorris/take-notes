afa47cb7264f9ae5218bce449bf4a696a33588c5
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-07-29 14:35:17 -0400

    update

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const fs = require("fs");
-const notes = require("./db/db.json");
 const path = require("path");
 const uuid = require("uuid");
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
 
 
 const app = express();
